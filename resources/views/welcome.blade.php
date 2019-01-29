6440299ceb029d344a37abdc5c9ac460bee8c635
rahul1101010082 <rahul.shukla@surmountsoft.com>
2019-01-29 18:18:43 +0530

    link addes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,6 +77,7 @@
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
                         @endif
+                        <a href="{{ route('uploadform') }}">Upload File</a>
                     @endauth
                 </div>
             @endif
